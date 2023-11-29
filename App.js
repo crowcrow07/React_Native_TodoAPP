09b9cfc9c15e3c21dedb42a24710036babe73734
crowcrow07 <skathd3402@gmail.com>
2023-11-29 17:13:01 +0900

    refactor : ListInput 컴포넌트 분리 feat : 공백은 글 생성 못하게 막기

--- App.js
+++ App.js
@@ -2,16 +2,13 @@ import { useState } from "react";
 import { Button, StyleSheet, TextInput, View, FlatList } from "react-native";
 
 import ListItem from "./components/ListItem";
+import ListInput from "./components/ListInput";
 
 export default function App() {
-  const [enteredListText, setEnteredListText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function listInputHandler(enteredText) {
-    setEnteredListText(enteredText);
-  }
-
-  function addListHandler() {
+  function addListHandler(enteredListText) {
+    if (!enteredListText.trim()) return;
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: enteredListText, key: Date.now() },
@@ -20,14 +17,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="한글도 괜찮죠?"
-          onChangeText={listInputHandler}
-        />
-        <Button title="Add List" onPress={addListHandler} />
-      </View>
+      <ListInput addListHandler={addListHandler} />
       <View style={styles.listContainer}>
         <FlatList
           data={courseGoals}
@@ -46,29 +36,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
   listContainer: {
     flex: 5,
-  },
-  listItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
   },
 });
--- components/ListInput.js
+++ components/ListInput.js
@@ -1,1 +1,47 @@
-export default function listInputHandler() {}
+import { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+
+export default function ListInput({ addListHandler }) {
+  const [enteredListText, setEnteredListText] = useState("");
+
+  function listInputHandler(enteredText) {
+    setEnteredListText(enteredText);
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        value={enteredListText}
+        style={styles.textInput}
+        placeholder="한글도 괜찮죠?"
+        onChangeText={listInputHandler}
+      />
+      <Button
+        title="Add List"
+        onPress={() => {
+          addListHandler(enteredListText);
+          setEnteredListText("");
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
