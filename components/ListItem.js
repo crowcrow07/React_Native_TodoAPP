8679e73d6e1c02c75942d608d2153a7a8e20bca1
crowcrow07 <skathd3402@gmail.com>
2023-12-04 16:19:16 +0900

    feat : 버튼 클릭시 물결 애니메이션

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,19 +2,26 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export default function ListItem({ id, text, onDeleteItem }) {
   return (
-    <Pressable onPress={() => onDeleteItem(id)}>
-      <View style={styles.listItem}>
-        <Text style={{ color: "white" }}>{text}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.listItem}>
+      <Pressable
+        android_ripple={{ color: "#481677" }}
+        onPress={() => onDeleteItem(id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.listText}>{text}</Text>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   listItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  listText: { color: "white", padding: 8 },
 });
